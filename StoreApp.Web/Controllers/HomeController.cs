903b728c38b5231fdddc99cea83adc7669ad3231
canersinan00 <canersinan1013@gmail.com>
2025-10-23 14:12:37 +0300

    Pagination

--- StoreApp.Web/Controllers/HomeController.cs
+++ StoreApp.Web/Controllers/HomeController.cs
@@ -7,20 +7,24 @@ namespace StoreApp.Web.Controllers;
   
 public class HomeController : Controller
 {
+    public int pageSize = 3;
     private IStoreRepository _storeRepository;
     public HomeController(IStoreRepository storeRepository)
     {
         _storeRepository = storeRepository;
     }
-    public IActionResult Index()
+    public IActionResult Index(int page = 1)
     {
-        var products = _storeRepository.Products.Select(p=> new ProductViewModel
-        {
-            Id = p.Id,
-            Name = p.Name,
-            Description = p.Description,
-            Price = p.Price
-        }).ToList();
+        var products = _storeRepository
+            .Products
+            .Skip((page - 1) * pageSize) //1-1 => 0 * 3 = 0   //2-1 => 1 * 3 = 3
+            .Select(p=> new ProductViewModel
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Description = p.Description,
+                Price = p.Price
+            }).Take(pageSize);
 
         return View(new ProductListViewModel
         {
