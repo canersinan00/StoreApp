f1f40e79cc36895bca2df0ab614abf76862db444
canersinan00 <canersinan1013@gmail.com>
2025-10-30 20:20:09 +0300

    Order Details Page Create and Added

---
+++ StoreApp.Web/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreApp.Web.Models;
+
+namespace StoreApp.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private Cart cart;
+
+        public OrderController(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View(new OrderModel(){Cart = cart});
+        }
+    }
+}
